54e6e8d6bc665acf88bf5f424a8f7d7b740b1896
NordSpaik <Spaik_mail@inbox.ru>
2023-02-08 02:03:54 +0300

    add def first_letter_upper() + docstrings

---
+++ str_func.py
@@ -0,0 +1,10 @@
+def upper_words(words):
+"""Преобразует все буквы в строке на заглавные"""
+
+    return words
+
+def first_letter_upper(words):
+"""Преобразует только первые буквы в строке в заглавные"""
+
+    return first_words
+
